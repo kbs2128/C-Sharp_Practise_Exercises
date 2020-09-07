a958dcc14d0c89b456a77360b304719a41613819
Bhavani Shankar Kedala <kbsbunny780@gmail.com>
2020-09-07 22:22:03 +0530

    MyfirstDLL

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Myfirstdll;
+using A = System.Console;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c1 = new Class1();
+           A.WriteLine( c1.Add(5, 3));
+        }
+    }
+}
